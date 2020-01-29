99bdc5397f50a9e74a883170d0519109077f858d
nmrhard <nmrhard@gmail.com>
2020-01-29 19:41:20 +0200

    Add redner pin function

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
 'use strict';
 
 var OFFERS = 8;
+var PIN_WIDTH = 65;
+var PIN_HEIGHT = 87;
 var TYPE_APARTMENT = ['palace', 'flat', 'house', 'bungalo'];
 var CHECKIN = ['12:00', '13:00', '14:00'];
 var CHECKOUT = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
-var PRICE ={
+var PRICE = {
   MIN: 1000,
   MAX: 10000
 };
+var GUESTS = {
+  MIN: 0,
+  MAX: 2
+}
+var ROOMS = {
+  MIN: 1,
+  MAX: 3
+}
 var LOCATION = {
   X: {
     MIN: 1,
@@ -20,6 +30,11 @@ var LOCATION = {
     MAX: 630
   }
 };
+
+var map  = document.querySelector('.map');
+map.classList.remove('map--faded');
+
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
 var getPictureNumber = function (offer) {
   return (offer >= 10) ? offer : '0' + offer;
@@ -60,8 +75,8 @@ var createOffer = function (numberOfOffer) {
       address: locationX + ',' +  locationY,
       price: getRendomNumber(PRICE.MIN, PRICE.MAX),
       type: TYPE_APARTMENT[getRendomElement(TYPE_APARTMENT)],
-      rooms: getRendomNumber(1, 5),
-      guests: getRendomNumber(1, 6),
+      rooms: getRendomNumber(ROOMS.MIN, ROOMS.MAX),
+      guests: getRendomNumber(GUESTS.MIN, GUESTS.MAX),
       checkin: CHECKIN[getRendomElement(CHECKIN)],
       checkout: CHECKOUT[getRendomElement(CHECKOUT)],
       features: getRandomArr(FEATURES),
@@ -76,3 +91,18 @@ var createOffer = function (numberOfOffer) {
 
   return offer;
 };
+
+var renderPin = function(offer) {
+  var pinElement = pinTemplate.cloneNode(true);
+  var pinX = offer.location.x - PIN_WIDTH / 2;
+  var pinY = offer.location.y + PIN_HEIGHT;
+
+  pinElement.setAttribute('style', 'left: ' + pinX  + 'px; ' + 'top: ' + pinY + 'px;');
+
+  pinElement.querySelector('img').src = offer.author.avatar;
+  pinElement.querySelector('img').alt = offer.offer.title;
+
+  return pinElement;
+}
+
+renderPin(createOffer(3));
